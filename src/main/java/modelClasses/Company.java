f928ae8d7d87d8d1cca681c888fd0229dbff38c6
thadjiantonis <theohadjiantonis@gmail.com>
2024-04-08 10:47:51 +0300

    -- Add setters to company class

--- src/main/java/modelClasses/Company.java
+++ src/main/java/modelClasses/Company.java
@@ -20,16 +20,28 @@ public class Company {
     public CompanyNames getName() {
         return companyName;
     }
+    public void setName(CompanyNames companyName) {
+        this.companyName = companyName;
+    }
 
     public int getNumberOfEmployees() {
         return numberOfEmployees;
+    }
+    public void setNumberOfEmployees(int numberOfEmployees) {
+        this.numberOfEmployees = numberOfEmployees;
     }
 
     public int getNumberOfCustomers() {
         return numberOfCustomers;
     }
+    public void setNumberOfCustomers( int numberOfCustomers) {
+        this.numberOfCustomers = numberOfCustomers;
+    }
 
     public Countries getCountry() {
         return country;
+    }
+    public void setCountry(Countries country) {
+        this.country = country;
     }
 }
